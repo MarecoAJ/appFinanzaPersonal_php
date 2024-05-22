b3de97ed4610abef873bec83c3a01e0f9359783c
Alejandro J Mareco <a.j.mareco.cit@gmail.com>
2024-05-22 11:17:41 -0300

    manejo de errores

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set("log_errors", 1);
+ini_set("error_log", "C:/xampp/htdocs/appFinanzaPersonal_php/php_error_lg");
+
 require_once "controllers/control_rutas.php";
 
 $inicio = new ControladorRutas();

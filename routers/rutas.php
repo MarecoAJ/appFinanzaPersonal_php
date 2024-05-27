cc8124656a0bdd4287492dc3713610281043bf8c
MarecoAJ <alejo_mareco@hotmail.com>
2024-05-27 20:31:06 -0300

    obtencion de rutas por uri

--- routers/rutas.php
+++ routers/rutas.php
@@ -1,7 +1,27 @@
 <?php
+$rutaObtenida = explode("/", $_SERVER['REQUEST_URI']);
+$rutaObtenida = array_filter($rutaObtenida);
+
+if($rutaObtenida == 0){
+
     $mensaje = [
-        'status' => 200,
-        'response' => "Bienvenidos",
+        'status' => 404,
+        'response' => "No encontrado",
     ];
 
-    echo json_encode($mensaje);
+    echo json_encode($mensaje, http_response_code($mensaje["status"]));
+ 
+} else{
+
+    switch($_SERVER["REQUEST_METHOD"]){
+        case 'GET':
+        $mensaje = [
+            'status' => 200,
+            'response' => "Solicitud correcta",
+        ];
+    
+        echo json_encode($mensaje, http_response_code($mensaje["status"]));
+        break;
+    }
+}
+?>
